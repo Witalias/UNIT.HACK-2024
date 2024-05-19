bd3ca1ac2d65b58a252420cc3261ce618a12957a
af Luv <vivaticomba@gmail.com>
2024-05-19 17:42:30 +0500

    generation

---
+++ Moon/Assets/Scripts/SpawnSouls.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSouls : MonoBehaviour
+{
+   
+    public GameObject souls = null;
+    public List<Transform> spawnPoints;
+
+    void Start()
+    {   
+        if (spawnPoints.Count != 0) 
+        {
+            Spawn(); 
+        }
+    }
+
+    void Spawn()
+    {
+         for ( int i = 0; i < 4; i++) 
+        { 
+            var spawn = Random.Range(0, spawnPoints.Count);
+            if (spawnPoints.Count == 0) 
+            {
+                break;
+            }
+            Instantiate(souls, spawnPoints[spawn].transform.position, Quaternion.identity);
+            spawnPoints.RemoveAt(spawn);
+            
+        }
+    }
+}
